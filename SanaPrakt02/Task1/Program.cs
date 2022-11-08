0eb17a19256a21a3a9ca4e7318ace730493708ea
Denis Yakovenko <kn202_yadv@student.ztu.edu.ua>
2022-11-08 22:24:52 +0200

    Change first three line in Task1

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
+Console.WriteLine("Звичайний текст у першому рядку");
+Console.WriteLine("Простий текс у другому рядку");
+Console.WriteLine("The simple text in 3-th line");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
