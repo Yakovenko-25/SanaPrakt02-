0b48547699be4a60a1fa9a9db80bb95d7502d5d4
Denis Yakovenko <kn202_yadv@student.ztu.edu.ua>
2022-11-08 22:18:08 +0200

    Make 5 duplicates operators in Task1

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcam");
